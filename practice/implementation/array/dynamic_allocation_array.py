88745ce8166d0a281839b7584d64c13f1db62835
Zhaoyu Zhang <zhangzhaoyu@yahoo.com>
2019-10-13 23:08:47 +0100

    add dynamic array

---
+++ practice/implementation/array/dynamic_allocation_array.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+""" 实现一个支持动态扩容的数组
+
+https://blog.csdn.net/weixin_43633501/article/details/90108817
+
+Properties：
+    capacity
+    size
+Functions:
+    add()
+    remove()
+
+
+
+"""
+
+
+class DynamicArray:
+    def __init__(self, capacity):
+        self.capacity = capacity
+
+    def add(self):
+        pass
+
+    def delete(self):
+        pass
+
+
