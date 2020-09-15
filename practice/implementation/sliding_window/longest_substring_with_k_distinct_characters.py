172ffbae60689720f86d4e5c03461d7ae9fb320e
Zhaoyu Zhang <zhangzhaoyu@yahoo.com>
2020-09-16 00:53:06 +0800

    added smalles subarray with a given sum k

---
+++ practice/implementation/sliding_window/longest_substring_with_k_distinct_characters.py
@@ -0,0 +1,23 @@
+"""
+Grokking the Coding Interview: Patterns for Coding Questions
+Sliding window - Longest Substring with K Distinct Characters (medium)
+
+Description:
+Given a string, find the length of the longest substring 
+in it with no more than K distinct characters.
+
+Input: String="araaci", K=2
+Output: 4
+Explanation: The longest substring with no more than '2' distinct characters is "araa".
+
+Time Complexity - O(N)
+Space Complexity - O(1)
+
+LeetCode link:
+"""
+
+def longest_substring_with_k_distinct_characters(string, k):
+    pass
+
+if __name__=="__main__":
+    pass
--- practice/implementation/sliding_window/smallest_subarray_with_a_given_sum.py
+++ practice/implementation/sliding_window/smallest_subarray_with_a_given_sum.py
@@ -14,25 +14,30 @@ Explanation: The smallest subarray with a sum great than or equal to '7' is [5, 
 
 Time Complexity - O(N)
 Space Complexity - O(1)
+
+Relevatn LeetCode link (Hard): https://leetcode.com/problems/shortest-subarray-with-sum-at-least-k/
 """
 
 
-def smallest_subarray_with_given_sum(arr, s):
-    collection = []
-    for window_size in range(1, len(arr) + 1):
-        window_sum = 0
-        window_start = 0
+def smallest_subarray_with_given_sum(s, arr):
+    window_sum = 0
+    window_start = 0
+    minimum_length = len(arr) + 1
 
-        for window_end in range(len(arr)):
-            window_sum += arr[window_end]
-            if window_end >= (window_size - 1):
-                if window_sum >= s:
-                    return window_size
-                window_sum -= arr[window_start]
-                window_start += 1
+    for window_end in range(len(arr)):
+        window_sum += arr[window_end]
 
-    return 0
+        while window_sum >= s:
+            minimum_length = min(minimum_length, window_end - window_start + 1)
+            window_sum -= arr[window_start]
+            window_start += 1
+
+    if minimum_length == len(arr) + 1:
+        return 0
+    return minimum_length
 
 
-result = smallest_subarray_with_given_sum([2, 1, 5, 2, 3, 2], s=7)
-print(result)
+if __name__=="__main__":
+    print("Smallest subarray length: " + str(smallest_subarray_with_given_sum(7, [2, 1, 5, 2, 3, 2])))
+    print("Smallest subarray length: " + str(smallest_subarray_with_given_sum(7, [2, 1, 5, 2, 8])))
+    print("Smallest subarray length: " + str(smallest_subarray_with_given_sum(8, [3, 4, 1, 1, 6])))
