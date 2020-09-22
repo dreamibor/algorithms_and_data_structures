3fa5d8f112e31febd2cecd54b7355ff76336f29d
Zhaoyu Zhang <zhangzhaoyu@yahoo.com>
2020-09-23 07:46:03 +0800

    longest substring

--- practice/implementation/sliding_window/longest_substring_with_k_distinct_characters.py
+++ practice/implementation/sliding_window/longest_substring_with_k_distinct_characters.py
@@ -16,8 +16,28 @@ Space Complexity - O(1)
 LeetCode link:
 """
 
-def longest_substring_with_k_distinct_characters(string, k):
-    pass
+def longest_substring_with_k_distinct(string, k):
+    window_start = 0
+    maximum_length = 0
+    char_set = set()
+
+    for window_end in range(len(string)):
+        char_set.add(string[window_end])
+        
+        while len(char_set) > k:
+            window_start += 1
+            char_set = set(string[window_start:window_end+1])
+        
+        maximum_length = max(maximum_length, window_end-window_start+1)
+        
+        print(string[window_start:window_end+1])    
+        print(char_set)
+        print(maximum_length)
+    return maximum_length
+
 
 if __name__=="__main__":
-    pass
+    print("Length of the longest substr1ing: " + str(longest_substring_with_k_distinct("araaci", 2)))
+    print("Length of the longest substr1ing: " + str(longest_substring_with_k_distinct("araaci", 1)))
+    print("Length of the longest substr1ing: " + str(longest_substring_with_k_distinct("cbbebi", 3)))
+    print("Length of the longest substr1ing: " + str(longest_substring_with_k_distinct("aa", 3)))
