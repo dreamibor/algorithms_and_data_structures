2105b51d62cd7833b9d4208c9fc85282e3abf53c
Zhaoyu Zhang <zhangzhaoyu@yahoo.com>
2020-11-25 14:12:02 +0800

    added all linked list problems

--- practice/implementation/linked_list/copy_list_with_random_pointer.py
+++ practice/implementation/linked_list/copy_list_with_random_pointer.py
@@ -2,25 +2,159 @@
 Linked List - Partition List (medium)
 
 Description:
-A linked list is given such that each node contains an additional random pointer which could point to any node in the list or null.
+A linked list is given such that each node contains an additional random pointer 
+which could point to any node in the list or null.
 
-Return a deep copy of the list.
+Return a `deep copy` of the list.
 
-The Linked List is represented in the input/output as a list of n nodes. Each node is represented as a pair of [val, random_index] where:
-
-    val: an integer representing Node.val
-    random_index: the index of the node (range from 0 to n-1) where random pointer points to, or null if it does not point to any node.
-
+The Linked List is represented in the input/output as a list of n nodes. Each node 
+is represented as a pair of [val, random_index] where:
+1. val: an integer representing Node.val
+2. random_index: the index of the node (range from 0 to n-1) where random pointer 
+points to, or null if it does not point to any node.
 
 Example:
-Input: head = [[7,null],[13,0],[11,4],[10,2],[1,0]]
+Input: [[7,null],[13,0],[11,4],[10,2],[1,0]]
 Output: [[7,null],[13,0],[11,4],[10,2],[1,0]]
 
 LeetCode link: https://leetcode-cn.com/problems/copy-list-with-random-pointer/
 """
+
 
 class Node:
     def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
         self.val = int(x)
         self.next = next
         self.random = random
+
+    def __repr__(self) -> str:
+        return f"[{self.val}, {self.next}]"
+
+def copy_random_list_hash_table(head: Node) -> Node:
+    """ Hash Table
+    We can use a hash table to save original nodes and copied nodes. 
+    We iterate the list for first time save all nodes and copied nodes 
+    into the hash table. The key will be the original node, while the 
+    value will be the newly created node (copied node with next and 
+    random as None).
+
+    Then we can iterate the original list to update all next and random 
+    pointers for those newly created nodes, as:
+    map.get(original) -> returns the newly copied node.
+    map.get(original.next) -> returns the newly copied node's next.
+    map.get(original.random) -> returns the newly copied node's random.
+
+    So we can assign those nodes to:
+    1. node_map[current].next = node_map[current.next]
+    2. node_map[current].random = node_map[current.random]
+
+    Finally, we can return the result by map.get(head).
+
+    Time Complexity - O(N) - For iteration twice.
+    Space Complexity - O(N) - For the hash table.
+    """
+    # Edge case - for empty list.
+    if not head: return None
+
+    node_map = {}
+    current = head
+
+    # Iterate through the linked list, add all nodes into hash table 
+    # with original node as key and newly created node (copied node) 
+    # as value.
+    while current:
+        new_node = Node(current.val, None, None)
+        node_map[current] = new_node
+        current = current.next
+
+    current = head
+
+    # Update those newly created nodes' next and random.
+    # The original node's next will be a key to get the newly copied 
+    # node's next. It will be the same for random pointer.
+    while current:
+        if current.next:
+            node_map[current].next = node_map[current.next]
+        
+        if current.random:
+            node_map[current].random = node_map[current.random]
+
+        current = current.next
+    
+    return node_map[head]
+
+
+def copy_random_list(head: Node) -> Node:
+    """ Copy after Original Nodes and Then Split
+
+    The most difficult part of this problem is how to copy the random 
+    pointer.
+
+    We can solve the problem with three steps:
+    1. Iterate through original nodes, create a copy node after each 
+    original node, so the original node 1 is pointing to newly created 
+    copy node 1' rather than orginal node 2.
+    2. Set new nodes' random pointers.
+    - If the original node 1's random pointer is pointing to the original 
+    node 3, then the random pointer of new copy node 1' is pointing to 
+    the original node 3's next. In general, the original i's random pointer 
+    is pointing to original node j, then the copied node i's random pointer 
+    will point to j's next.
+    3. Split the list into the original list and the copied list and then 
+    return the new list.
+
+    Time Complexity - O(N) - Iterate the list.
+    Space Complexity - O(1) - Constant space for pointers.
+    """
+    # Edge case - empty list
+    if not head: return head
+
+    # Create copied nodes after original nodes.
+    # 1 -> 1' -> 2 -> 2' -> 3 -> 3'
+    current = head
+    while current:
+        new_node = Node(current.val, None, None)
+        new_node.next = current.next
+        current.next = new_node
+        current = new_node.next
+    
+    # Set random pointers for new copied nodes.
+    current = head
+    while current:
+        if current.random:
+            current.next.random = current.random.next
+        current = current.next.next
+
+    # Split two lists
+    # 1  -> 2  -> 3
+    # 1' -> 2' -> 3'
+    current = head
+    dummy = Node(-1, None, None)
+    cur = dummy
+
+    while current:
+        cur.next = current.next
+        cur = cur.next
+        current.next = cur.next
+        current = current.next
+    
+    return dummy.next
+
+
+if __name__ == "__main__":
+    head = Node(3)
+    head.next = Node(2)
+    head.random = None
+    head.next.next = Node(1)
+    head.next.random = head
+    head.next.next.random = None
+
+    new_head = copy_random_list_hash_table(head)
+    while new_head:
+        print(new_head)
+        new_head = new_head.next
+    
+    new_head = copy_random_list(head)
+    while new_head:
+        print(new_head)
+        new_head = new_head.next
--- practice/implementation/linked_list/partition_list.py
+++ practice/implementation/linked_list/partition_list.py
@@ -2,14 +2,54 @@
 Linked List - Partition List (medium)
 
 Description:
-Given a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
-
-You should preserve the original relative order of the nodes in each of the two partitions.
+Given a linked list and a value x, partition it such that all nodes less than x come before 
+nodes greater than or equal to x.
 
 Example:
 Input: head = 1->4->3->2->5->2, x = 3
 Output: 1->2->2->4->3->5
 
+Note:
+You should preserve the original relative order of the nodes in each of the two partitions.
+
 LeetCode link: https://leetcode-cn.com/problems/partition-list/
 """
 
+from linked_list import ListNode, create_linked_list, traverse
+
+
+def partition_list(head: ListNode, x: int) -> ListNode:
+    """ Break Down and Concatenate
+    We can break down the list into two parts:
+    1. Nodes have value smaller than x;
+    2. Nodes have value larger than or equal to x.
+
+
+    Time Complexity - O(N) - Iterate through the list once.
+    Space Complexty - O(1) - For pointers.
+    """
+    # Use two dummy nodes to store two break-down lists.
+    s = smaller = ListNode(-1)
+    l = larger = ListNode(-1)
+
+    while head:
+        if head.val < x:
+            s.next = head
+            s = s.next
+        else:
+            l.next = head
+            l = l.next
+        head = head.next
+
+    # Last node of larger is the end. 
+    l.next = None
+    # Concatenate two lists, as smaller will be followed by larger.
+    s.next = larger.next
+
+    return smaller.next
+    
+
+if __name__ == "__main__":
+    head = create_linked_list([1,4,3,2,5,2])
+    result = partition_list(head, 3)
+    print(traverse(result))
