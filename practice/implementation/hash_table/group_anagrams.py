b9d56d7004840b2c4d6320ee78be985d0caca003
jaszha02 <jason.zhang@arm.com>
2021-04-01 21:24:37 +0100

    add hash table problems

--- practice/implementation/hash_table/group_anagrams.py
+++ practice/implementation/hash_table/group_anagrams.py
@@ -1,1 +1,86 @@
-https://leetcode.com/problems/group-anagrams/
+"""
+Hash Table - Group Anagrams
+
+Description:
+Given an array of strings strs, group the anagrams together. You can return the answer 
+in any order. An Anagram is a word or phrase formed by rearranging the letters of a 
+different word or phrase, typically using all the original letters exactly once.
+
+Example:
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],
+        ["nat","tan"],
+        ["ate","eat","tea"]]
+
+LeetCode: https://leetcode-cn.com/problems/group-anagrams/
+"""
+from collections import defaultdict
+
+
+def group_anagrams_array(strings: str) -> list:
+    """ Count the character frequency in an array and use it as the hash table key.
+
+    Time Complexity - O(N*(K + |Σ|)) - N is the number of strings in the input, K is the 
+    maximum length of strings, while |Σ| is the number of characters, here we only have 
+    26 lowercase letters. We are going to iterate through N strings, For each string, we 
+    will use O(K) time to calculate the frequency of each character, and then we need O(|Σ|) 
+    to convert the array into hash table key and update the hash table with O(1).
+    Space Complexity - O(N * (K + |Σ|)) - Using hash table to save all the strings.
+    """
+    anagram_map = defaultdict(list)
+
+    for string in strings:
+        # Use the character as index of the array.
+        counts = [0] * 26
+        # Count the frequency of each character.
+        for char in string:
+            counts[ord(char) - ord("a")] += 1
+        
+        # Convert the list to tuple, so it can be a key of hash table.
+        anagram_map[tuple(counts)].append(string)
+    
+    # Return all the values in the hash table as list.
+    return list(anagram_map.values())
+
+
+def group_anagrams_sort(strings: str) -> list:
+    """ Sort the string as the hash table key.
+
+    Time Complexity - O(N*K*logK) - N is the number of strings, K is the maximum length of 
+    the string. We iterate each string once and need O(K*logK) to sort the string.
+    Space Complexity - O(NK) - For the hash table.
+    """
+    anagram_map = defaultdict(list)
+
+    for string in strings:
+        # sorted(string) will return a list of characters. so we need 
+        # to join them together as a string to be the key for the hash 
+        # table. 
+        key = "".join(sorted(string))
+        anagram_map[key].append(string)
+    
+    # Return all the values in the hash table as list.
+    return list(anagram_map.values())
+
+
+
+if __name__ == "__main__":
+    # Hash table
+    strs = ["eat","tea","tan","ate","nat","bat"]
+    print(group_anagrams_array(strs))
+
+    strs = [""]
+    print(group_anagrams_array(strs))
+
+    strs = ["a"]
+    print(group_anagrams_array(strs))
+
+    # Sort
+    strs = ["eat","tea","tan","ate","nat","bat"]
+    print(group_anagrams_sort(strs))
+
+    strs = [""]
+    print(group_anagrams_sort(strs))
+
+    strs = ["a"]
+    print(group_anagrams_sort(strs))
--- practice/implementation/hash_table/valid_anagram.py
+++ practice/implementation/hash_table/valid_anagram.py
@@ -1,1 +1,91 @@
- https://leetcode.com/problems/valid-anagram/
+"""
+Hash Table - Valid Anagram (easy)
+
+Description:
+Given two strings s and t, return true if t is an anagram of s, and false 
+otherwise.
+
+Example:
+Input: s = "anagram", t = "nagaram"
+Output: true
+
+LeetCode: https://leetcode-cn.com/problems/valid-anagram/
+"""
+from collections import defaultdict
+
+
+def is_anagram_hash_table(source:str, target:str) -> bool:
+    """ Using hash table to count the frequency of characters.
+
+    Time Complexity - O(N) - Scanning through the input once.
+    Space Complexity - O(1) - the space for hash table will remain the same
+    no matter how large N is.
+    """
+    # Create two hash table for source and target string.
+    source_map = defaultdict(int)
+    target_map = defaultdict(int)
+
+    # Count the frequency of characters in the string.
+    for char in source:
+        source_map[char] += 1
+    
+    for char in target:
+        target_map[char] += 1
+
+    # Compare the two hash tables. 
+    return source_map == target_map
+
+def is_anagram_hash_array(source:str, target:str) -> bool: 
+    """ Using an array to replace hash table.
+        
+    Time Complexity - O(N) - Scanning through the input once.
+    Space Complexity - O(1) - the space for the array (hash table) will remain 
+    the same no matter how large N is.
+    """
+    if len(source) != len(target):
+        return False
+    
+    # Create an array as hash table, assuming the string only 
+    # contains 26 lowercase letters.
+    table = [0]*26
+
+    for char in source:
+        table[ord(char) - ord("a")] += 1
+    
+    for char in target:
+        table[ord(char) - ord("a")] -= 1
+        if table[ord(char) - ord("a")] < 0:
+            return False
+    
+    return True
+
+def is_anagram_sort(source:str, target:str) -> bool:
+    """ Sort the string and then compare.
+
+    Time Complexity - O(NlogN) - for quick sort.
+    Space Complexity - O(1) - Depending on the sorting algorithm, the space 
+    complexity could be auxiliary O(1) for heap sort.
+    """
+    return sorted(source) == sorted(target)
+
+if __name__ == "__main__":
+    # Hash table
+    s, t = "anagram", "nagaram"
+    print(is_anagram_hash_table(s, t))
+
+    s, t = "car", "rat"
+    print(is_anagram_hash_table(s, t))
+
+    # Array (hash table)
+    s, t = "anagram", "nagaram"
+    print(is_anagram_hash_array(s, t))
+
+    s, t = "car", "rat"
+    print(is_anagram_hash_array(s, t))
+
+    # Sort and compare
+    s, t = "anagram", "nagaram"
+    print(is_anagram_sort(s, t))
+
+    s, t = "car", "rat"
+    print(is_anagram_sort(s, t))
